bafcd17c9629df72a320ae450a096dda33fb9387
Michael Rackowski <MichaelRackowski@gmail.com>
2019-08-20 14:10:32 -0500

    Added new functions and cleaned up the code

--- Diceproject.js
+++ Diceproject.js
@@ -1,12 +1,11 @@
 function runGame(){
-	let totalHp = 100
-	while(totalHp >= 1){
-	let result = prompt("heal or attack");
+	let player1Health = 100;
+	let player2Health = 100;
+	let playerTurn = 1;
 	var dice;
 	let sidesAttack = [20,8,4];
 	let sidesHeal = [8];
 	let sidesDefend =[12,10,6];
-
 	let attack;
 	let attackOne;
 	let attackTwo;
@@ -22,66 +21,99 @@ function runGame(){
 	let resultDefenseThree;
 	let minValue;
 	let maxValue;
+	let resultHeal
+	let resultAttackThree
 
-	 if(result == "heal"){
-	heal = roll(sidesHeal[0]);
-}
-
-
-
-	else if(result == "attack"){
+	while(player1Health >= 0 && player2Health >= 0){
 	
-	attack = roll(sidesAttack[0]);
-	 attackOne = roll(sidesAttack[1]);
-	  attackTwo = roll(sidesAttack[2]);
+	 if(playerTurn == 1){
+		let result = prompt(" player 1: heal or attack?");
 
-	  resultAttackTwo = (attack * attackTwo)
-	  resultMultiply = roll(sidesAttack[1])
-	}
+		if(result == "heal"){
+			heal = roll(sidesHeal[0]);
+		}
+		else if(result == "attack"){
+			attack = roll(sidesAttack[0]);
+	 		attackOne = roll(sidesAttack[1]);
+	  		attackTwo = roll(sidesAttack[2]);
 
-	 
-	  
-
-	  if(resultMultiply % 2 == 0){
-	  	console.log("Its a hit!!!")
-	  }
-
-	  	else if(resultMultiply % 2 ==1){
-	  		console.log("You missed your attack!!!")
-	  	}
+	 		resultAttackTwo = (attack * attackTwo);
+	  		resultMultiply = roll(sidesAttack[1]);
+		}
 	
+		if(resultMultiply % 2 == 0){
+	  		console.log("Its a hit!!!")
+		}
+		else if(resultMultiply % 2 ==1){
+  			console.log("You missed your attack!!!")
+		}
+	  	
+		resultTwo = "defense";
+		defense = roll(sidesDefend[0]);
+		defenseOne = roll(sidesDefend[1]);
+		defenseTwo = roll(sidesDefend[2]);
 
+		resultDodge = roll(sidesDefend[1]);
+		resultDefenseTwo =(defense + defenseTwo);
 
-	
-
-	
-
-
-	
-	 resultTwo = "defense";
-	 defense = roll(sidesDefend[0]);
-	 defenseOne = roll(sidesDefend[1]);
-	 defenseTwo = roll(sidesDefend[2]);
-
-
-	 resultDodge = roll(sidesDefend[1]);
-	resultDefenseTwo =(defense + defenseTwo);
-
-	 if(resultDodge % 2 == 0){
-	  	console.log("You dodged there attack!")
-	  }
-
-	  	else if(resultDodge % 2 ==1){
+		if(resultDodge % 2 == 0){
+	  		console.log("You dodged there attack!")
+		}
+		else if(resultDodge % 2 ==1){
 	  		console.log("You weren't fast enough to dodge there attack!")
-	  	}
-	  	maxValue= max(resultAttackTwo, resultDefenseTwo);
-	  	minValue = min(resultAttackTwo, resultDefenseTwo);
+		}
+		maxValue= max(resultAttackTwo, resultDefenseTwo);
+		minValue = min(resultAttackTwo, resultDefenseTwo);
 
+		resultDefenseThree = maxValue - minValue;
+		player2Health -= resultDefenseThree;
+	}
+	else{
+		let result2 = prompt("player 2: heal or attack?");
+	
+		if(result2 == "heal"){
+			heal = roll(sidesHeal[0]);
+		}
+		else if(resultAttackThree == "attack"){
+			attack = roll(sidesAttack[0]);
+			attackOne = roll(sidesAttack[1]);
+			attackTwo = roll(sidesAttack[2]);
 
-	  	resultDefenseThree = maxValue - minValue;
-	  	totalHp -= resultDefenseThree;
-	  	}	
+			resultAttackTwo = (attack * attackTwo);
+			resultMultiply = roll(sidesAttack[1]);
+		}
+
+		if(resultMultiply % 2 == 0){
+				console.log("Its a hit!!!")
+		}
+		else if(resultMultiply % 2 ==1){
+				console.log("You missed your attack!!!")
+		}
+		resultTwo = "defense";
+		defense = roll(sidesDefend[0]);
+		defenseOne = roll(sidesDefend[1]);
+		defenseTwo = roll(sidesDefend[2]);
+		resultDodge = roll(sidesDefend[1]);
+		resultDefenseTwo =(defense + defenseTwo);
+
+		if(resultDodge % 2 == 0){
+		  	console.log("You dodged there attack!")
+		}
+		else if(resultDodge % 2 ==1){
+		  	console.log("You weren't fast enough to dodge there attack!")
+		}
+		maxValue= max(resultAttackTwo, resultDefenseTwo);
+		minValue = min(resultAttackTwo, resultDefenseTwo);
+
+		resultDefenseThree = maxValue - minValue;
+		player2Health -= resultDefenseThree;
+	}
 }
+
+	  	}	
+		
+
+
 
 function roll(numOfSides){
 			var randomNumber = Math.floor(Math.random()*numOfSides)+1;
@@ -97,6 +129,7 @@ function min(a, b){
 		return a;
 	else return b;
 }
+
 
 
 runGame();
